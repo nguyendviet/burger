3bc70e35c75db5a72c03016935a355f572b0f378
Viet Nguyen <vietjul@gmail.com>
2017-11-10 11:38:22 -0500

    update sql

--- db/schema.sql
+++ db/schema.sql
@@ -5,8 +5,8 @@ USE burgers_db;
 
 CREATE TABLE burgers (
     id INT(11) AUTO_INCREMENT NOT NULL,
-    burger_name VARCHAR(50) NOT NULL,
+    burger_name VARCHAR(255) NOT NULL,
     devoured BOOL DEFAULT 0,
-    date TIMESTAMP,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
 );
